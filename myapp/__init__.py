b4cb73d593fe766396df3a1c7aafc4fc9401796b
ivslvs <irnvslvs@gmail.com>
2020-09-19 23:02:08 +0300

    Fix pep8

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
+
 app = Flask(__name__)
 
 
--- myapp/worker.py
+++ myapp/worker.py
@@ -1,6 +1,7 @@
 import os
 from celery import Celery
 
+
 env = os.environ
 CELERY_BROKER_URL = env.get('CELERY_BROKER_URL', 'redis://redis:6379'),
 CELERY_RESULT_BACKEND = env.get('CELERY_RESULT_BACKEND', 'redis://redis:6379')
